66ddc5f7f4d9aad3be79d01e14037434494f25f5
Jatin0977 <jkdevil1212@gmail.com>
2026-02-21 19:48:59 +0530

    Initial commit

---
+++ data/testimonial.js
@@ -0,0 +1,50 @@
+export const testimonial = [
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "Nadir Ali",
+    image: "/photo/img1.png",
+    role: "Marketing Manager",
+    company: "Tech Giant Co.",
+  },
+  {
+    quote:
+      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+    author: "Faaiz Khan ",
+    image: "/photo/IMG2.png",
+    role: "Data Analyst",
+    company: "StartUp Inc.",
+  },
+  {
+    quote:
+      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+    author: "Mohammed Humza ",
+    image: "/photo/img3.png",
+    role: "Software Developer",
+    company: "Accenture Pvt Ltd.",
+  },
+  {
+    quote:
+      "I landed a senior position with a 40% salary increase thanks to the career coaching insights provided here!",
+    author: "Radhika Rathore",
+    image: "/photo/img4.png",
+    role: "Web Designer",
+    company: "Giant Co.",
+  },
+  {
+    quote:
+      "The AI interview practice covered everything I needed. Went from nervous to confident in two weeks!",
+    author: "Mahak Jurel",
+    image: "/photo/img5.png",
+    role: "Product Manager",
+    company: "StartUp Inc.",
+  },
+  {
+    quote:
+      "Combined the interview prep, resume optimization, and cover letter generator. Got 3 job offers!",
+    author: "Jatin Kumar",
+    image: "/photo/img6.png",
+    role: "Marketing Director",
+    company: "Global Corp",
+  },
+];
